88ad9f5c49b2d1e3faeb932db1943e2a493aafc7
Ben Sully <ben.sully@grafana.com>
2022-08-21 14:01:39 +0100

    Use cargo feature to toggle recompiling protoc definitions

--- build.rs
+++ build.rs
@@ -1,14 +1,21 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = prost_build::Config::new();
-    config.bytes(&[
-        ".pluginv2.CallResourceRequest",
-        ".pluginv2.CallResourceResponse",
-        ".pluginv2.RunStreamRequest",
-        ".pluginv2.SubscribeStreamRequest",
-    ]);
-    Ok(tonic_build::configure().out_dir("src/pluginv2").compile_with_config(
-        config,
-        &["./vendor/proto/backend.proto"],
-        &["./vendor/proto"],
-    )?)
+    #[cfg(feature = "gen-proto")]
+    {
+        let mut config = prost_build::Config::new();
+        config.bytes(&[
+            ".pluginv2.CallResourceRequest",
+            ".pluginv2.CallResourceResponse",
+            ".pluginv2.RunStreamRequest",
+            ".pluginv2.SubscribeStreamRequest",
+        ]);
+        Ok(tonic_build::configure()
+            .out_dir("src/pluginv2")
+            .compile_with_config(
+                config,
+                &["./vendor/proto/backend.proto"],
+                &["./vendor/proto"],
+            )?)
+    }
+    #[cfg(not(feature = "gen-proto"))]
+    Ok(())
 }
