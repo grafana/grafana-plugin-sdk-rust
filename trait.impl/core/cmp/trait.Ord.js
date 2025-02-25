b88528b068fc36bedcb06b9fd7974735bb0ba7fd
sd2k <sd2k@users.noreply.github.com>
2025-02-25 20:12:44 +0000

    deploy: 5bcf1bd7371fbacdad0648e09e051a164cdd8c82

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["grafana_plugin_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/admission_request/enum.Operation.html\" title=\"enum grafana_plugin_sdk::pluginv2::admission_request::Operation\">Operation</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/check_health_response/enum.HealthStatus.html\" title=\"enum grafana_plugin_sdk::pluginv2::check_health_response::HealthStatus\">HealthStatus</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/publish_stream_response/enum.Status.html\" title=\"enum grafana_plugin_sdk::pluginv2::publish_stream_response::Status\">Status</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/subscribe_stream_response/enum.Status.html\" title=\"enum grafana_plugin_sdk::pluginv2::subscribe_stream_response::Status\">Status</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1327]}
---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["grafana_plugin_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/backend/enum.ErrorSource.html\" title=\"enum grafana_plugin_sdk::backend::ErrorSource\">ErrorSource</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/admission_request/enum.Operation.html\" title=\"enum grafana_plugin_sdk::pluginv2::admission_request::Operation\">Operation</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/check_health_response/enum.HealthStatus.html\" title=\"enum grafana_plugin_sdk::pluginv2::check_health_response::HealthStatus\">HealthStatus</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/publish_stream_response/enum.Status.html\" title=\"enum grafana_plugin_sdk::pluginv2::publish_stream_response::Status\">Status</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"grafana_plugin_sdk/pluginv2/subscribe_stream_response/enum.Status.html\" title=\"enum grafana_plugin_sdk::pluginv2::subscribe_stream_response::Status\">Status</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1638]}
