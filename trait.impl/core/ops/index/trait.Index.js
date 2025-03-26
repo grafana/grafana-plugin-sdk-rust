f5cfb7fce52d096af8ce2615fc89e1bd575d1aa7
sd2k <sd2k@users.noreply.github.com>
2025-03-26 21:05:09 +0000

    deploy: 2a6bf0928b42ff3ea9cf2f4120033413d3b96963

---
+++ trait.impl/core/ops/index/trait.Index.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["grafana_plugin_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"grafana_plugin_sdk/data/struct.Frame.html\" title=\"struct grafana_plugin_sdk::data::Frame\">Frame</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"grafana_plugin_sdk/data/struct.Frame.html\" title=\"struct grafana_plugin_sdk::data::Frame\">Frame</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[815]}
